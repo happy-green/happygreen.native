0ef63bcdfda050d47c6d2edbc390632074dd7e50
Abhi10699 <patelabhi10699@gmail.com>
2019-07-18 21:27:58 +0530

    wrote a class based component

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React, {Fragment} from 'react';
 import {View,Text} from 'react-native';
 import Navigator from './src/navigator';
 
-const App = () => {
-  return(
-    <Navigator/>
-  )
+export default class App extends React.Component{
+  render(){
+    return(
+      <Navigator/>
+    )
+  }
 }
-
-export default Navigator;
